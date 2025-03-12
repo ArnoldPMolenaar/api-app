cdceade3880aaceef14ac8aa84738223054028c5
Arnold Molenaar <arnoldp1991@gmail.com>
2025-03-12 08:02:15 +0100

    🚀 perf: add initial project structure with db and middleware setup
    Changelog: performance

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"api-app/main/src/cache"
+	"api-app/main/src/configs"
+	"api-app/main/src/database"
+	"api-app/main/src/middleware"
+	"fmt"
+	routeutil "github.com/ArnoldPMolenaar/api-utils/routes"
+	"github.com/ArnoldPMolenaar/api-utils/utils"
+	"github.com/gofiber/fiber/v2"
+	"os"
+)
+
+func main() {
+	// Define Fiber config.
+	config := configs.FiberConfig()
+
+	// Define a new Fiber app with config.
+	app := fiber.New(config)
+
+	// Register Fiber's middleware for app.
+	middleware.FiberMiddleware(app)
+
+	// Open database connection.
+	if err := database.OpenDBConnection(); err != nil {
+		panic(fmt.Sprintf("Could not connect to the database: %v", err))
+	}
+
+	// Open Valkey connection.
+	if err := cache.OpenValkeyConnection(); err != nil {
+		panic(fmt.Sprintf("Could not connect to the cache: %v", err))
+	}
+	defer cache.Valkey.Close()
+
+	// Register route for 404 Error.
+	routeutil.NotFoundRoute(app)
+
+	// Start server (with or without graceful shutdown).
+	if os.Getenv("STAGE_STATUS") == "dev" {
+		utils.StartServer(app)
+	} else {
+		utils.StartServerWithGracefulShutdown(app)
+	}
+}
